993314110e79cf71f287789711c6c3d6a6df6298
Felix Green <work@felix.green>
2021-01-13 22:33:40 -0500

    push the limits of what SwiftUI can render

--- VisaViz/Shared/GridView.swift
+++ VisaViz/Shared/GridView.swift
@@ -18,8 +18,23 @@ struct GridView_Previews: PreviewProvider {
 struct GridView: View {
 	// MARK: - PROPS
 	
+	struct Config {
+		var quantity: Int = 100
+		var size: CGFloat = 10.0
+		var space: CGFloat = 2.0
+	}
+	
+	var config = Config()
+	
 	// MARK: - BODY
 	var body: some View {
-		Text("Hello, World!")
+		LazyVGrid(columns: [GridItem(.adaptive(minimum: config.size, maximum: 100.0), spacing: config.space)], spacing: config.space, content: {
+			ForEach(0..<config.quantity) { n in
+				Rectangle()
+					.foregroundColor(.blue)
+					.frame(height: config.size)
+			}
+		})
+		.frame(maxWidth: .infinity, maxHeight: .infinity)
 	}
 }
