5ce1f91fdef26b3984ec896f63a20e46a19cf31b
Felix Green <work@felix.green>
2021-01-13 22:33:40 -0500

    push the limits of what SwiftUI can render

--- VisaViz/Shared/ContentView.swift
+++ VisaViz/Shared/ContentView.swift
@@ -17,8 +17,37 @@ struct ContentView_Previews: PreviewProvider {
 struct ContentView: View {
 	// MARK: - PROPS
 	
+	@State var gridViewConfig = GridView.Config(
+		quantity: 100000,
+		size: 4,
+		space: 1
+	)
+	
+	@State var show = false
+	
 	// MARK: - BODY
 	var body: some View {
-		GridView()
+		VStack {
+			HStack {
+//				Text("testz")
+				Button("\(show ? "hide()" :  "show()")") {
+					show.toggle()
+				}
+//				Button("set q to 100") {
+//					gridViewConfig.quantity = 100
+//				}
+//				Button("set q to 1000") {
+//					gridViewConfig.quantity = 1000
+//				}
+			}
+			Divider()
+			if show {
+				GridView(config: gridViewConfig)
+					.drawingGroup()
+			} else {
+				Spacer()
+			}
+		}
+		.frame(minWidth: 1000, minHeight: 800)
 	}
 }
