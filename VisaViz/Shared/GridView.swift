425cbf4b794f754844e2a09e0db56a317ff95a54
Felix Green <work@felix.green>
2021-01-13 20:30:09 -0500

    new xcode project

---
+++ VisaViz/Shared/GridView.swift
@@ -0,0 +1,20 @@
+//
+//  GridView.swift
+//  VisaViz
+//
+//  Created by Felix Akira Green on 1/13/21.
+//
+
+import SwiftUI
+
+struct GridView: View {
+	var body: some View {
+		Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+	}
+}
+
+struct GridView_Previews: PreviewProvider {
+	static var previews: some View {
+		GridView()
+	}
+}
