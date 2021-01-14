b103d7fdf084d1fee1511e980004050ef9b5ba04
Felix Green <work@felix.green>
2021-01-13 20:30:09 -0500

    new xcode project

---
+++ VisaViz/Shared/VisaVizApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisaVizApp.swift
+//  Shared
+//
+//  Created by Felix Akira Green on 1/13/21.
+//
+
+import SwiftUI
+
+@main
+struct VisaVizApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
