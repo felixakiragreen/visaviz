db89e898bd0ad2117ca3897670fea5d8c19b4685
Felix Green <work@felix.green>
2021-01-15 20:44:00 -0500

    pass through hoveredTweet

--- VisaViz/Shared/ContentView.swift
+++ VisaViz/Shared/ContentView.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
 		NavigationView {
-		ContentView(tweets: .constant(Tweet.previewData))
+			ContentView(
+				tweets: .constant(Tweet.previewData),
+				hoveredTweet: .constant(nil)
+			)
 		}
 	}
 }
@@ -20,40 +23,58 @@ struct ContentView: View {
 	// MARK: - PROPS
 	
 	@Binding var tweets: [Tweet]
+	@Binding var hoveredTweet: Tweet?
 	
 	@State var gridViewConfig = GridView.Config(
 		quantity: 100,
 		size: 36,
 		space: 6
 	)
-	
-	@State var show = true
+
 	
 	// MARK: - BODY
 	var body: some View {
 		VStack {
 			Text("sidebar")
 			Divider()
-			Button("\(show ? "hide()" :  "show()")") {
-				show.toggle()
+			
+			Text("Sorting → Date.Asc")
+			WIP(label: "sorting selection", vertical: true)
+//			Picker(selection: $config.coloring.mode, label: ArrowLabel("mode")) {
+//				Text("single").tag(ColorMode.single)
+//				Text("main").tag(ColorMode.main)
+//				Text("group").tag(ColorMode.group)
+//				Text("random").tag(ColorMode.random)
+//			}.pickerStyle(SegmentedPickerStyle())
+			
+			Spacer()
+			
+			if let tweet = hoveredTweet  {
+				
+				Text("\(tweet.createdAt, formatter: DateFormatter.mediumDateTimeFormatter)")
+				Text("\(tweet.fullText)")
 			}
 			
-			
+			Spacer()
 			
 		}//: Sidebar
 		VStack {
 			GeometryReader { geometry in
-				if show {
+//				if show {
 					ScrollView {
-					GridView(tweets: $tweets, config: gridViewConfig)
+						GridView(
+							tweets: $tweets,
+							hoveredTweet: $hoveredTweet,
+							config: gridViewConfig
+						)
 	//					.drawingGroup()
 					}
-				} else {
-					Spacer()
-				}
+//				} else {
+//					Spacer()
+//				}
 			}
 		}//: MainView
-		.padding()
+//		.padding()
 //		.frame(minWidth: 1000, minHeight: 800)
 	}
 	
