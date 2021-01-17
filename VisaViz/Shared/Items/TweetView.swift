a78bdda4ab59bf27d13484961aae644b4630d2d8
Felix Green <work@felix.green>
2021-01-17 13:51:27 -0500

    add metrics + link to Tweet preview

--- VisaViz/Shared/Items/TweetView.swift
+++ VisaViz/Shared/Items/TweetView.swift
@@ -22,15 +22,40 @@ struct TweetView: View {
 	
 	// MARK: - BODY
 	var body: some View {
-		VStack(alignment: .leading) {
+		let hue = tweet.hue ?? .grey
+		
+		VStack(alignment: .leading, spacing: 8) {
 			HStack {
 				Text("\(tweet.createdAt, formatter: DateFormatter.bestDateFormatter)")
 				Spacer()
 				Text("\(tweet.createdAt, formatter: DateFormatter.bestTimeFormatter)")
 			}
 			.font(.caption)
-			.foregroundColor(.secondary)
+			.foregroundColor(ColorPreset(hue: hue, lum: .medium).getColor())
 			Text("\(tweet.fullText)")
+			HStack {
+				HStack(spacing: 16) {
+					Label {
+						Text("\(tweet.metrics.likes)")
+					} icon: {
+						Image(systemName: "heart.fill")
+							.foregroundColor(.secondary)
+					}
+					Label {
+						Text("\(tweet.metrics.retweets)")
+					} icon: {
+						Image(systemName: "repeat")
+							.foregroundColor(.secondary)
+					}
+				}
+				Spacer()
+				Link(
+					destination: URL(string: "https://twitter.com/i/web/status/\(tweet.id)")!,
+					label: {
+						Text("open")
+						Image(systemName: "square.and.arrow.up")
+				})
+			}
 		}
 		.padding()
 		.background(
@@ -39,7 +64,10 @@ struct TweetView: View {
 					ColorPreset(hue: .grey, lum: .extraLight).getColor()
 				)
 		)
+		.overlay(
+			RoundedRectangle(cornerRadius: 8.0, style: .continuous)
+				.strokeBorder(ColorPreset(hue: hue, lum: .extraLight).getColor(), lineWidth: 2, antialiased: true)
+		)
 //		.clipShape(RoundedRectangle(cornerRadius: 25.0, style: .continuous))
-		
 	}
 }
