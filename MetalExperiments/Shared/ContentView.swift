4be2a7a0e7c3d90602b1654047ee5b161973e6df
Felix Green <work@felix.green>
2021-06-22 13:07:00 -0400

    try geting the params & camera data

--- MetalExperiments/Shared/ContentView.swift
+++ MetalExperiments/Shared/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import struct Forge.ForgeView
+import Satin
 
 // MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
@@ -30,13 +31,39 @@ struct ContentView: View {
 				.frame(minWidth: 512, minHeight: 512)
 //				.edgesIgnoringSafeArea(.all)
 //			SubContentView(text: $textEntry)
-//			VStack {
-//				Text("text → \(textEntry)")
-//				Button("UPDATE") {
-//					textEntry = "dudeness"
-//				}
-//			}
-//			.padding()
+			VStack {
+				Text("text")
+				Button("log") {
+				
+					if let camera = renderer.cameraController.camera {
+						print(camera.top, camera.right, camera.bottom, camera.left)
+						print(renderer.cameraController.camera?.position)
+					}
+					
+					let paramsMap: [String: Parameter]
+					paramsMap = renderer.instanceMaterial.parameters.paramsMap
+					
+					let bla = paramsMap.mapValues { parameter -> Double in
+						if parameter is FloatParameter {
+							let param = parameter as! FloatParameter
+							let value = Double(param.value)
+							return value
+						}
+						else if parameter is IntParameter {
+							let param = parameter as! IntParameter
+							let value = Double(param.value)
+							return value
+						}
+						else {
+							return Double(0)
+						}
+					}
+					print(bla)
+					// print(renderer)
+				}
+			}
+			.padding()
+			.background(Color.red)
 		}
 		
 		.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
