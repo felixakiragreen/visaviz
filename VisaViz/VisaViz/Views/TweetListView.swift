c4c81412d2c7e631f085487f4583149bb5345640
Felix Green <work@felix.green>
2022-08-25 14:28:01 -0400

    display number of tweets loaded

--- VisaViz/VisaViz/Views/TweetListView.swift
+++ VisaViz/VisaViz/Views/TweetListView.swift
@@ -5,16 +5,37 @@
 //  Created by Felix Akira Green on 8/25/22.
 //
 
+import Atoms
 import SwiftUI
 
 struct TweetListView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	
+	@WatchStateObject(TweetArchiveAtom())
+	var archive
+	
+	var body: some View {
+		VStack {
+			Text("hey")
+			Text("tweets: \(archive.allTweets.count)")
+		}
+		.onAppear {
+			appear()
+		}
+	}
+	
+	@MainActor
+	func appear() {
+		Task {
+			await archive.load()
+		}
+	}
 }
 
 struct TweetListView_Previews: PreviewProvider {
-    static var previews: some View {
-        TweetListView()
-    }
+	static var previews: some View {
+		AtomRoot {
+			TweetListView()
+				.preferredColorScheme(.dark)
+		}
+	}
 }
