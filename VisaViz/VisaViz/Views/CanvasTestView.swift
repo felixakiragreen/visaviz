7c09f078d7ddb688d254b3d671a3ee5ff4441144
Felix Green <work@felix.green>
2022-08-25 15:25:49 -0400

    formatting

--- VisaViz/VisaViz/Views/CanvasTestView.swift
+++ VisaViz/VisaViz/Views/CanvasTestView.swift
@@ -12,41 +12,40 @@ struct CanvasTestView: View {
 		VStack {
 			Canvas { context, size in
 				let containerWidth = size.width
-				
+
 				let count = 100
 				let columnCount = count
 				let rowCount = count
-				
+
 				let cellSize = containerWidth / CGFloat(count)
 				let cellPadding = cellSize / 10
-				
+
 				let colors = [
 					Color.blue,
 					Color.green,
 					Color.yellow,
 					Color.orange,
 					Color.red,
-					Color.purple
+					Color.purple,
 				]
-				
+
 				for columnIndex in 0 ..< columnCount {
 					for rowIndex in 0 ..< rowCount {
-						
 						let cellPath = Path(CGRect(
 							x: CGFloat(columnIndex) * cellSize + cellPadding,
 							y: CGFloat(rowIndex) * cellSize + cellPadding,
 							width: cellSize - cellPadding * 2,
 							height: cellSize - cellPadding * 2
 						))
-						
+
 						let index = columnIndex + (rowIndex * columnCount)
 						let color = colors[index % colors.count]
-						
+
 						context.fill(cellPath, with: .color(color))
 					}
 				}
-			}
-		}
+			} // Canvas
+		} // VStack
 		// .frame(width: 2000, height: 2000)
 		.frame(width: 500, height: 500)
 		// .frame(maxWidth: .infinity, maxHeight: .infinity)
