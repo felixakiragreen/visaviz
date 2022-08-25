efbf708e791743821300013b32184e8f8cb35936
Felix Green <work@felix.green>
2022-08-25 15:24:59 -0400

    create Hue

--- VisaViz/VisaViz/Model/Hue.swift
+++ VisaViz/VisaViz/Model/Hue.swift
@@ -5,4 +5,14 @@
 //  Created by Felix Akira Green on 8/25/22.
 //
 
-import Foundation
+import SwiftUI
+
+enum Hue: String, CaseIterable {
+	case grey, blue, green, yellow, orange, red, purple
+}
+
+extension Color {
+	init(_ hue: Hue, _ level: Int) {
+		self.init("\(hue.rawValue).\(level)")
+	}
+}
