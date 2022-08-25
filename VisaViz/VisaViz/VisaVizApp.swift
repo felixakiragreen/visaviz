c3e7e043a26cfe0d4f68286e409afbd438db0a9d
Felix Green <work@felix.green>
2022-08-25 14:28:21 -0400

    add atoms

--- VisaViz/VisaViz/VisaVizApp.swift
+++ VisaViz/VisaViz/VisaVizApp.swift
@@ -5,13 +5,16 @@
 //  Created by Felix Akira Green on 2022.7.28.
 //
 
+import Atoms
 import SwiftUI
 
 @main
 struct VisaVizApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	var body: some Scene {
+		WindowGroup {
+			AtomRoot {
+				ContentView()
+			}
+		}
+	}
 }
