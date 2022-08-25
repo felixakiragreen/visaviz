df7e12da2a2a4c7d4eb0bfef97d79dc5fd695db2
Felix Green <work@felix.green>
2022-08-25 15:25:59 -0400

    render the tweet visuals

--- VisaViz/VisaViz/Views/TweetCanvasView.swift
+++ VisaViz/VisaViz/Views/TweetCanvasView.swift
@@ -5,16 +5,67 @@
 //  Created by Felix Akira Green on 8/25/22.
 //
 
+import Atoms
 import SwiftUI
 
 struct TweetCanvasView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	
+	@WatchStateObject(TweetArchiveAtom())
+	var archive
+	
+	@Watch(TweetVisualsAtom())
+	var visuals
+	
+	var body: some View {
+		VStack {
+			Canvas { context, size in
+				let containerWidth = size.width
+
+				let count = 32
+				let columnCount = count
+				let rowCount = count
+
+				let cellSize = containerWidth / CGFloat(count)
+				let cellPadding = cellSize / 10
+
+				// let colors = [
+				// 	Color.blue,
+				// 	Color.green,
+				// 	Color.yellow,
+				// 	Color.orange,
+				// 	Color.red,
+				// 	Color.purple,
+				// ]
+
+				for columnIndex in 0 ..< columnCount {
+					for rowIndex in 0 ..< rowCount {
+						let cellPath = Path(CGRect(
+							x: CGFloat(columnIndex) * cellSize + cellPadding,
+							y: CGFloat(rowIndex) * cellSize + cellPadding,
+							width: cellSize - cellPadding * 2,
+							height: cellSize - cellPadding * 2
+						))
+
+						let index = columnIndex + (rowIndex * columnCount)
+						if visuals[optional: index] != nil {
+							let color = Color(visuals[index].hue, 400)
+							
+							context.fill(cellPath, with: .color(color))
+						}
+					}
+				}
+			} // Canvas
+		} // VStack
+		.frame(width: 500, height: 500)
+		// .frame(maxWidth: .infinity, maxHeight: .infinity)
+	}
 }
 
 struct TweetCanvasView_Previews: PreviewProvider {
-    static var previews: some View {
-        TweetCanvasView()
-    }
+	static var previews: some View {
+		AtomRoot {
+			TweetCanvasView()
+				.preferredColorScheme(.dark)
+		}
+	}
 }
