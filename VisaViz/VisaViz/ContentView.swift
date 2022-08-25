04f271553d49ae040eccdbd4d459222b45f699d6
Felix Green <work@felix.green>
2022-08-25 15:26:22 -0400

    display new tweet canvas

--- VisaViz/VisaViz/ContentView.swift
+++ VisaViz/VisaViz/ContentView.swift
@@ -5,23 +5,33 @@
 //  Created by Felix Akira Green on 2022.7.28.
 //
 
+import Atoms
 import SwiftUI
 
 struct ContentView: View {
+
+	@WatchStateObject(TweetArchiveAtom())
+	var archive
+
 	var body: some View {
 		VStack {
 			ScrollView {
 				TweetListView()
-				CanvasTestView()
+				TweetCanvasView()
 			}
 			.frame(maxWidth: .infinity, maxHeight: .infinity)
 		}
 		.frame(maxWidth: .infinity, maxHeight: .infinity)
 	}
+
+	
 }
 
 struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
-		ContentView()
+		AtomRoot {
+			ContentView()
+				.preferredColorScheme(.dark)
+		}
 	}
 }
