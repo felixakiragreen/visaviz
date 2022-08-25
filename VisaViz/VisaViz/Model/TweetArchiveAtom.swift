7ce8c13fd61f104fe9ff846790cd74b3b7d5fc0b
Felix Green <work@felix.green>
2022-08-25 14:27:51 -0400

    create an atom for the object

--- VisaViz/VisaViz/Model/TweetArchiveAtom.swift
+++ VisaViz/VisaViz/Model/TweetArchiveAtom.swift
@@ -5,4 +5,12 @@
 //  Created by Felix Akira Green on 8/25/22.
 //
 
+import Atoms
 import Foundation
+
+struct TweetArchiveAtom: ObservableObjectAtom, KeepAlive, Hashable {	
+	func object(context: Context) -> TweetArchiveStore {
+		TweetArchiveStore()
+	}
+}
+
