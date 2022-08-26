30c8e03ee4ad786ba8b926ff8b8e00ecab49a7d3
Felix Green <work@felix.green>
2022-08-26 11:25:30 -0400

    add grid state atom

--- VisaViz/VisaViz/Model/GridAtom.swift
+++ VisaViz/VisaViz/Model/GridAtom.swift
@@ -5,4 +5,16 @@
 //  Created by Felix Akira Green on 8/26/22.
 //
 
+
+import Atoms
 import Foundation
+
+struct GridAtom: StateAtom, Hashable {
+	func defaultValue(context: Context) -> GridParams {
+		return GridParams()
+	}
+}
+
+struct GridParams: Hashable {
+	var columns: Int = 100
+}
