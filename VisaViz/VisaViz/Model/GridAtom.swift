c0c5d00f603944e3b437158dfa04ffef7cf535eb
Felix Green <work@felix.green>
2022-08-26 13:26:24 -0400

    store the height as well

--- VisaViz/VisaViz/Model/GridAtom.swift
+++ VisaViz/VisaViz/Model/GridAtom.swift
@@ -22,6 +22,7 @@ struct GridParams: Hashable {
 	static let minRows = 10
 
 	var containerWidth: CGFloat = 0
+	var containerHeight: CGFloat = 0
 	var cellCount: Int = 0
 	
 	var cellWidth: CGFloat = 0
@@ -38,13 +39,16 @@ struct GridParams: Hashable {
 		rows = max(GridParams.minRows, rowsMax)
 	}
 	
-	mutating func calcCells(width: CGFloat) {
-		print("calcCells, w: \(width)")
-		containerWidth = width
-		cellWidth = containerWidth / CGFloat(columns)
+	@inlinable
+	mutating func calcCells(size: CGSize) {
+		print("calcCells, w:\(size.width) h:\(size.height)")
+		containerWidth = size.width
+		containerHeight = size.height
+		cellWidth = size.width / CGFloat(columns)
 		cellPadding = cellWidth / 10
 	}
 	
+	@inlinable
 	mutating func recalcCells() {
 		print("recalcCells, w: \(containerWidth)")
 		cellWidth = containerWidth / CGFloat(columns)
