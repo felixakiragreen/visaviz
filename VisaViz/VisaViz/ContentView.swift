5589df521ee8f1820887ccff7acbf906df448c23
Felix Green <work@felix.green>
2022-08-26 13:13:26 -0400

    track mouse position using hover

--- VisaViz/VisaViz/ContentView.swift
+++ VisaViz/VisaViz/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
 	@WatchStateObject(TweetArchiveAtom())
 	var archive
 
+	@State var hover: CGPoint?
+
 	var body: some View {
 		VStack {
 			TweetListView()
@@ -20,23 +22,34 @@ struct ContentView: View {
 			GeometryReader { geo in
 				ScrollView {
 					TweetCanvasView(
-						containerWidth: geo.size.width
+						containerWidth: geo.size.width,
+						hover: hover
 					)
-						.frame(maxWidth: .infinity, minHeight: geo.size.height)
+					.frame(maxWidth: .infinity, minHeight: geo.size.height)
 				}
 			}
 			.frame(maxWidth: .infinity, maxHeight: .infinity)
+			.mouseMove(isInside: { inside in
+				print("is inside: \(inside)")
+				if inside == false {
+					hover = nil
+				}
+			}, onMove: { location in
+				print("location: \(location)")
+				hover = location
+			})
+			
+			TweetHoverView(hover: hover)
 		}
-		.background(Color(.red, 400).opacity(0.2))
+		// .background(Color(.red, 400).opacity(0.2))
 		.frame(maxWidth: .infinity, maxHeight: .infinity)
 	}
 }
 
 struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
-		AtomRoot {
-			ContentView()
-				.preferredColorScheme(.dark)
-		}
+		ContentView()
+			.embedAtomRoot()
+			.preferredColorScheme(.dark)
 	}
 }
