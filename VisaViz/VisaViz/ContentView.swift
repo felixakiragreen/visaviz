e891a67a55061a549a54571a21caf9788b994724
Felix Green <work@felix.green>
2022-08-26 11:25:42 -0400

    measure view with geometry

--- VisaViz/VisaViz/ContentView.swift
+++ VisaViz/VisaViz/ContentView.swift
@@ -9,22 +9,27 @@ import Atoms
 import SwiftUI
 
 struct ContentView: View {
-
 	@WatchStateObject(TweetArchiveAtom())
 	var archive
 
 	var body: some View {
 		VStack {
-			ScrollView {
-				TweetListView()
-				TweetCanvasView()
+			TweetListView()
+				.padding()
+
+			GeometryReader { geo in
+				ScrollView {
+					TweetCanvasView(
+						containerWidth: geo.size.width
+					)
+						.frame(maxWidth: .infinity, minHeight: geo.size.height)
+				}
 			}
 			.frame(maxWidth: .infinity, maxHeight: .infinity)
 		}
+		.background(Color(.red, 400).opacity(0.2))
 		.frame(maxWidth: .infinity, maxHeight: .infinity)
 	}
-
-	
 }
 
 struct ContentView_Previews: PreviewProvider {
