85d1b3223c302916bef08d742843209dad783c37
Felix Green <work@felix.green>
2022-08-26 11:28:20 -0400

    use the column count to display

--- VisaViz/VisaViz/Views/TweetCanvasView.swift
+++ VisaViz/VisaViz/Views/TweetCanvasView.swift
@@ -9,34 +9,34 @@ import Atoms
 import SwiftUI
 
 struct TweetCanvasView: View {
-	
 	@WatchStateObject(TweetArchiveAtom())
 	var archive
-	
+
 	@Watch(TweetVisualsAtom())
 	var visuals
 	
+	@WatchState(GridAtom())
+	var grid
+	
+	var containerWidth: CGFloat
+
 	var body: some View {
+		// .overlay(
+		// 	GeometryReader { geo in
+		// 		Color.clear.onAppear {
+		// 			contentSize = geo.size
+		// 		}
+		// 	}
+		// )
 		VStack {
+			let columnCount = grid.columns
+			let rowCountMax = Int(ceil(Double(visuals.count) / Double(columnCount)))
+			let rowCount = max(10, rowCountMax)
+			
+			let cellSize = containerWidth / CGFloat(columnCount)
+			let cellPadding = cellSize / 10
+			
 			Canvas { context, size in
-				let containerWidth = size.width
-
-				let count = 100
-				let columnCount = count
-				let rowCount = count
-
-				let cellSize = containerWidth / CGFloat(count)
-				let cellPadding = cellSize / 10
-
-				// let colors = [
-				// 	Color.blue,
-				// 	Color.green,
-				// 	Color.yellow,
-				// 	Color.orange,
-				// 	Color.red,
-				// 	Color.purple,
-				// ]
-
 				for columnIndex in 0 ..< columnCount {
 					for rowIndex in 0 ..< rowCount {
 						let cellPath = Path(CGRect(
@@ -49,22 +49,24 @@ struct TweetCanvasView: View {
 						let index = columnIndex + (rowIndex * columnCount)
 						if visuals[optional: index] != nil {
 							let color = Color(visuals[index].hue, 400)
-							
+
 							context.fill(cellPath, with: .color(color))
 						}
 					}
 				}
 			} // Canvas
+			.frame(minHeight: CGFloat(rowCount) * cellSize)
 		} // VStack
-		.frame(width: 600, height: 600)
+		// .frame(width: 600, height: 600)
 		// .frame(maxWidth: .infinity, maxHeight: .infinity)
+		.background(Color(.orange, 400).opacity(0.2))
 	}
 }
 
 struct TweetCanvasView_Previews: PreviewProvider {
 	static var previews: some View {
 		AtomRoot {
-			TweetCanvasView()
+			TweetCanvasView(containerWidth: 600)
 				.preferredColorScheme(.dark)
 		}
 	}
