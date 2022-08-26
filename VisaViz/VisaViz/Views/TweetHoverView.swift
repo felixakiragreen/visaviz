cf978e67859fa8ca28846003e6c4db451475b392
Felix Green <work@felix.green>
2022-08-26 13:13:47 -0400

    build simple tweet hover view

--- VisaViz/VisaViz/Views/TweetHoverView.swift
+++ VisaViz/VisaViz/Views/TweetHoverView.swift
@@ -5,16 +5,32 @@
 //  Created by Felix Akira Green on 8/26/22.
 //
 
+import Atoms
 import SwiftUI
 
 struct TweetHoverView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	
+	@WatchState(GridAtom())
+	var grid
+	
+	var hover: CGPoint?
+	
+	var body: some View {
+		VStack {
+			if let hover {
+				Text("Hover \(hover.x), \(hover.y)")
+			}
+			if hover == nil {
+				Text("No hover")
+			}
+		}
+	}
 }
 
 struct TweetHoverView_Previews: PreviewProvider {
-    static var previews: some View {
-        TweetHoverView()
-    }
+	static var previews: some View {
+		TweetHoverView()
+			.embedAtomRoot()
+			.preferredColorScheme(.dark)
+	}
 }
