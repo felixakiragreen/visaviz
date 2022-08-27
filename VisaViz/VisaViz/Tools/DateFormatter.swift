409c3c33e6b3dd9c3cf73983c1c70b33fe5b37ac
Felix Green <work@felix.green>
2022-08-26 22:01:18 -0400

    create files

---
+++ VisaViz/VisaViz/Tools/DateFormatter.swift
@@ -0,0 +1,8 @@
+//
+//  DatFormatter.swift
+//  VisaViz
+//
+//  Created by Felix Akira Green on 8/26/22.
+//
+
+import Foundation
---
+++ VisaViz/VisaViz/Views/TweetView.swift
@@ -0,0 +1,20 @@
+//
+//  TweetView.swift
+//  VisaViz
+//
+//  Created by Felix Akira Green on 8/26/22.
+//
+
+import SwiftUI
+
+struct TweetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TweetView_Previews: PreviewProvider {
+    static var previews: some View {
+        TweetView()
+    }
+}
