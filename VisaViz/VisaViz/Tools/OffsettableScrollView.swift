436ad9cd0a15c6a1ce8f5a975340ab41b9e8ebe5
Felix Green <work@felix.green>
2022-08-27 10:09:35 -0400

    add offsettable scroll view

--- VisaViz/VisaViz/Tools/OffsettableScrollView.swift
+++ VisaViz/VisaViz/Tools/OffsettableScrollView.swift
@@ -7,14 +7,48 @@
 
 import SwiftUI
 
-struct ScrollViewOffset: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+private struct OffsetPreferenceKey: PreferenceKey {
+	static var defaultValue: CGPoint = .zero
+
+	static func reduce(value _: inout CGPoint, nextValue _: () -> CGPoint) {}
 }
 
-struct ScrollViewOffset_Previews: PreviewProvider {
-    static var previews: some View {
-        ScrollViewOffset()
-    }
+struct OffsettableScrollView<T: View>: View {
+	let axes: Axis.Set
+	let showsIndicator: Bool
+	let onOffsetChanged: (CGPoint) -> Void
+	let content: T
+
+	init(
+		axes: Axis.Set = .vertical,
+		showsIndicator: Bool = true,
+		onOffsetChanged: @escaping (CGPoint) -> Void = { _ in },
+		@ViewBuilder content: () -> T
+	) {
+		self.axes = axes
+		self.showsIndicator = showsIndicator
+		self.onOffsetChanged = onOffsetChanged
+		self.content = content()
+	}
+
+	var body: some View {
+		ScrollView(axes, showsIndicators: showsIndicator) {
+			GeometryReader { proxy in
+				Color.clear.preference(
+					key: OffsetPreferenceKey.self,
+					value: proxy.frame(
+						in: .named("ScrollViewOrigin")
+					).origin
+				)
+			}
+			.frame(width: 0, height: 0)
+			content
+				.padding(.top, -8)
+		}
+		.coordinateSpace(name: "ScrollViewOrigin")
+		.onPreferenceChange(
+			OffsetPreferenceKey.self,
+			perform: onOffsetChanged
+		)
+	}
 }
