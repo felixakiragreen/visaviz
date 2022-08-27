128fd7de761175bdee1eb54233f5a4f90a21797a
Felix Green <work@felix.green>
2022-08-27 10:58:08 -0400

    add CG conformance

--- VisaViz/VisaViz/Tools/CG+Hashable.swift
+++ VisaViz/VisaViz/Tools/CG+Hashable.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+extension CGPoint: Hashable {
+	public func hash(into hasher: inout Hasher) {
+		hasher.combine(x)
+		hasher.combine(y)
+	}
+}
+
+extension CGSize: Hashable {
+	public func hash(into hasher: inout Hasher) {
+		hasher.combine(width)
+		hasher.combine(height)
+	}
+}
