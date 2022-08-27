2118170f69718baa7cfd0319a914657ec39d6554
Felix Green <work@felix.green>
2022-08-27 10:09:12 -0400

    create new file

---
+++ VisaViz/VisaViz/Tools/OffsettableScrollView.swift
@@ -0,0 +1,20 @@
+//
+//  ScrollViewOffset.swift
+//  VisaViz
+//
+//  Created by Felix Akira Green on 8/27/22.
+//
+
+import SwiftUI
+
+struct ScrollViewOffset: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScrollViewOffset_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollViewOffset()
+    }
+}
