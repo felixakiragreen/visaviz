57b0128d53178b07d8203a01918ec26fb34f06eb
Felix Green <work@felix.green>
2022-08-27 10:59:14 -0400

    only render boxes in view

--- VisaViz/VisaViz/Views/TweetCanvasView.swift
+++ VisaViz/VisaViz/Views/TweetCanvasView.swift
@@ -18,11 +18,13 @@ struct TweetCanvasView: View {
 	@WatchState(GridAtom())
 	var grid
 	
-	var container: CGSize
-	var containerWidth: CGFloat {
-		container.width
+	@WatchState(ScrollAtom())
+	var scroll
+	
+	var size: CGSize
+	var width: CGFloat {
+		size.width
 	}
-	var hover: CGPoint?
 
 	var body: some View {
 		VStack {
@@ -31,42 +33,57 @@ struct TweetCanvasView: View {
 			
 			let cellSize = grid.cellWidth
 			let cellPadding = grid.cellPadding
+			
+			let rowsInView = ceil(size.height / cellSize) + 2 // adding 2 for buffer
+			let rowsScrolled = floor(abs(scroll) / cellSize) - 1 // removing first
 
 			// let rowCountMax = Int(ceil(Double(visuals.count) / Double(columnCount)))
 			// let rowCount = max(10, rowCountMax)
 			//
-			// let cellSize = containerWidth / CGFloat(columnCount)
+			// let cellSize = width / CGFloat(columnCount)
 			// let cellPadding = cellSize / 10
 			
 			Canvas { context, size in
 				for columnIndex in 0 ..< columnCount {
 					for rowIndex in 0 ..< rowCount {
-						let cellPath = Path(CGRect(
-							x: CGFloat(columnIndex) * cellSize + cellPadding,
-							y: CGFloat(rowIndex) * cellSize + cellPadding,
-							width: cellSize - cellPadding * 2,
-							height: cellSize - cellPadding * 2
-						))
-
-						let index = columnIndex + (rowIndex * columnCount)
-						if visuals[optional: index] != nil {
-							let tweet = visuals[index]
-							let lit = 700 - (tweet.lit * 100)
-							let color = Color(tweet.hue, lit)
-
-							context.fill(cellPath, with: .color(color))
-						} else {
-							// context.fill(cellPath, with: .color(Color(.grey, 800)))
+						let row = CGFloat(rowIndex)
+						/// check if row is in View
+						if row > rowsScrolled,
+							row < rowsScrolled + rowsInView {
+							// context.fill(cellPath, with: .color(Color(.green, 500)))
+							
+							let cellPath = Path(CGRect(
+								x: CGFloat(columnIndex) * cellSize + cellPadding,
+								y: CGFloat(rowIndex) * cellSize + cellPadding,
+								width: cellSize - cellPadding * 2,
+								height: cellSize - cellPadding * 2
+							))
+							
+							let index = columnIndex + (rowIndex * columnCount)
+							if visuals[optional: index] != nil {
+								let tweet = visuals[index]
+								let lit = 700 - (tweet.lit * 100)
+								let color = Color(tweet.hue, lit)
+								
+								context.fill(cellPath, with: .color(color))
+							} else {
+								// context.fill(cellPath, with: .color(Color(.grey, 800)))
+							}
+							
+						}
+						/// else if not in view, DO NOT RENDER
+						else {
+							// context.fill(cellPath, with: .color(Color(.blue, 500)))
 						}
 					}
 				}
 			} // Canvas
 			.frame(minHeight: CGFloat(rowCount) * cellSize)
 			.onAppear {
-				grid.calcCells(size: container)
+				grid.calcCells(size: size)
 			}
-			.onChange(of: container) {
-				if grid.containerWidth != $0.width || grid.containerHeight != $0.height {
+			.onChange(of: size) {
+				if grid.container != $0 {
 					grid.calcCells(size: $0)
 				}
 			}
@@ -77,7 +94,9 @@ struct TweetCanvasView: View {
 
 struct TweetCanvasView_Previews: PreviewProvider {
 	static var previews: some View {
-		TweetCanvasView(container: CGSize(width: 600, height: 480))
+		TweetCanvasView(
+			size: CGSize(width: 600, height: 480)
+		)
 			.embedAtomRoot()
 			.preferredColorScheme(.dark)
 	}
