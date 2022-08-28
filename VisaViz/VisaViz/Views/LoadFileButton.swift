a4c8b31939145dfb50ed187b021abe780d6959d8
Felix Green <work@felix.green>
2022-08-28 10:04:51 -0400

    create new file

---
+++ VisaViz/VisaViz/Views/LoadFileButton.swift
@@ -0,0 +1,20 @@
+//
+//  LoadFileButton.swift
+//  VisaViz
+//
+//  Created by Felix Akira Green on 8/28/22.
+//
+
+import SwiftUI
+
+struct LoadFileButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoadFileButton_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadFileButton()
+    }
+}
