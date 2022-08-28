17312a77a15d0d4febbaded1b35de13e62fd2823
Felix Green <work@felix.green>
2022-08-28 10:05:13 -0400

    present dialog and load it

--- VisaViz/VisaViz/Views/LoadFileButton.swift
+++ VisaViz/VisaViz/Views/LoadFileButton.swift
@@ -5,12 +5,55 @@
 //  Created by Felix Akira Green on 8/28/22.
 //
 
+import Atoms
+import AppKit
 import SwiftUI
 
 struct LoadFileButton: View {
+	
+	@WatchStateObject(TweetArchiveAtom())
+	var archive
+	
+	@State private var isLoaderPresented: Bool = false
+	
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		 Button(
+			action: {
+				if !isLoaderPresented {
+					isLoaderPresented = true
+				}
+			},
+			label: {
+				Text("Load tweet.js")
+			}
+		 )
+		 .disabled(isLoaderPresented)
+		 .onChange(of: isLoaderPresented, perform: { presented in
+			 // binding changed from false to true
+			 if presented == true {
+				 let panel = NSOpenPanel()
+				 panel.allowsMultipleSelection = false
+				 panel.canChooseDirectories = false
+				 panel.canChooseFiles = true
+				 panel.begin { response in
+					 if response == .OK {
+						 // pickedCompletionHandler(panel.urls)
+						 print("picked", panel.urls)
+						 loadFromFile(url: panel.urls[0])
+					 }
+					 // reset the isPresented variable to false
+					 isLoaderPresented = false
+				 }
+			 }
+		 })
     }
+	
+	@MainActor
+	func loadFromFile(url: URL) {
+		Task {
+			await archive.loadFileURL(file: url)
+		}
+	}
 }
 
 struct LoadFileButton_Previews: PreviewProvider {
