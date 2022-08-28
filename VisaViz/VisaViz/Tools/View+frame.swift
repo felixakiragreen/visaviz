4521c5a9976a3478a860b81fa1ef0ee988b279a6
Felix Green <work@felix.green>
2022-08-28 09:43:00 -0400

    add framing helpers

--- VisaViz/VisaViz/Tools/View+frame.swift
+++ VisaViz/VisaViz/Tools/View+frame.swift
@@ -5,4 +5,22 @@
 //  Created by Felix Akira Green on 8/28/22.
 //
 
-import Foundation
+import SwiftUI
+
+extension View {
+	func frame(size: CGFloat, alignment: Alignment = .center) -> some View {
+		frame(
+			width: size,
+			height: size,
+			alignment: alignment
+		)
+	}
+	
+	func frame(size: CGSize, alignment: Alignment = .center) -> some View {
+		frame(
+			width: size.width,
+			height: size.height,
+			alignment: alignment
+		)
+	}
+}
