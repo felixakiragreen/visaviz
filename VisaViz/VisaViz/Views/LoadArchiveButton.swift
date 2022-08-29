90cdecb02da8c7233f1bd8e979ef83eed7a16e53
Felix Green <work@felix.green>
2022-08-29 19:39:38 -0400

    build out new load archive button

--- VisaViz/VisaViz/Views/LoadArchiveButton.swift
+++ VisaViz/VisaViz/Views/LoadArchiveButton.swift
@@ -2,19 +2,62 @@
 //  LoadArchiveButton.swift
 //  VisaViz
 //
-//  Created by Felix Akira Green on 8/29/22.
+//  Created by Felix Akira Green on 8/28/22.
 //
 
+import Atoms
+import AppKit
 import SwiftUI
 
 struct LoadArchiveButton: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	
+	@WatchStateObject(TweetArchiveAtom())
+	var archive
+	
+	@State private var isLoaderPresented: Bool = false
+	
+	var body: some View {
+		Button(
+			action: {
+				if !isLoaderPresented {
+					isLoaderPresented = true
+				}
+			},
+			label: {
+				Text("Open Archive")
+			}
+		)
+		.disabled(isLoaderPresented)
+		.onChange(of: isLoaderPresented, perform: { presented in
+			// binding changed from false to true
+			if presented == true {
+				let panel = NSOpenPanel()
+				panel.allowsMultipleSelection = false
+				panel.canChooseDirectories = true
+				panel.canChooseFiles = false
+				panel.begin { response in
+					if response == .OK {
+						print("response", panel.urls)
+						loadArchive(url: panel.urls[0])
+					}
+					// reset the isPresented variable to false
+					isLoaderPresented = false
+				}
+			}
+		})
+	}
+	
+	@MainActor
+	func loadArchive(url: URL) {
+		print("loadArchive", url)
+		Task {
+			await archive.loadArchive(path: url)
+		}
+	}
 }
 
 struct LoadArchiveButton_Previews: PreviewProvider {
-    static var previews: some View {
-        LoadArchiveButton()
-    }
+	static var previews: some View {
+		LoadArchiveButton()
+	}
 }
