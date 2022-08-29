9164bece2800930a6f13d25acb80cec69d23882d
Felix Green <work@felix.green>
2022-08-29 19:39:46 -0400

    build out the empty view

--- VisaViz/VisaViz/Views/EmptyView.swift
+++ VisaViz/VisaViz/Views/EmptyView.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 struct EmptyView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	let appVersionString: String = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+	
+	var body: some View {
+		VStack {
+			Text("Welcome to VisaViz v\(appVersionString)")
+				.font(.title)
+			
+			Text("How To Use")
+				.font(.title2)
+			
+			VStack {
+				
+				Text("You need to download your Twitter Archive FIRST")
+				Text("It will take ages.")
+			}
+			
+			
+			Text("Once you have, ")
+			Text("BIG OPEN ARCHIVE BUTTON")
+			
+			Text("Feedback")
+				.font(.title2)
+			
+			Text("There is a lot ")
+			
+		}
+	}
 }
 
 struct EmptyView_Previews: PreviewProvider {
-    static var previews: some View {
-        EmptyView()
-    }
+	static var previews: some View {
+		EmptyView()
+	}
 }
