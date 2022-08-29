46c651def935d6b073d2b03124fad6e5c7765274
Felix Green <work@felix.green>
2022-08-29 19:40:39 -0400

    extract loaded view into own view

--- VisaViz/VisaViz/Views/ArchiveLoadedView.swift
+++ VisaViz/VisaViz/Views/ArchiveLoadedView.swift
@@ -5,16 +5,53 @@
 //  Created by Felix Akira Green on 8/29/22.
 //
 
+import Atoms
 import SwiftUI
 
 struct ArchiveLoadedView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	@WatchStateObject(TweetArchiveAtom())
+	var archive
+	
+	@WatchState(ScrollAtom())
+	var scroll
+	
+	@State var hover: CGPoint?
+	
+	var body: some View {
+		ZStack {
+			GeometryReader { geo in
+				OffsettableScrollView(onOffsetChange: { point in
+					scroll = point.y
+				}) {
+					TweetCanvasView(
+						size: geo.size
+					)
+					.frame(maxWidth: .infinity, minHeight: geo.size.height)
+				}
+			} // GeometryReader
+			.frame(maxWidth: .infinity, maxHeight: .infinity)
+			
+			.mouseMove(isInside: { inside in
+				// print("is inside: \(inside)")
+				if inside == false {
+					hover = nil
+				}
+			}, onMove: { location in
+				// print("location: \(location)")
+				hover = location
+			})
+			
+			if hover != nil {
+				TweetHoverView(hover: hover!)
+			}
+		} // ZStack
+	}
 }
 
 struct ArchiveLoadedView_Previews: PreviewProvider {
-    static var previews: some View {
-        ArchiveLoadedView()
-    }
+	static var previews: some View {
+		ArchiveLoadedView()
+			.embedAtomRoot()
+			.preferredColorScheme(.dark)
+	}
 }
