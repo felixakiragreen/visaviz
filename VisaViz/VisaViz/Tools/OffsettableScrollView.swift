07c55666610d8de006e1e04e761cd096fe0dd611
Felix Green <work@felix.green>
2022-08-29 14:09:09 -0400

    cleanup OffsettableScrollView

--- VisaViz/VisaViz/Tools/OffsettableScrollView.swift
+++ VisaViz/VisaViz/Tools/OffsettableScrollView.swift
@@ -12,46 +12,44 @@ import SwiftUI
 
 private struct OffsetPreferenceKey: PreferenceKey {
 	static var defaultValue: CGPoint = .zero
-
 	static func reduce(value _: inout CGPoint, nextValue _: () -> CGPoint) {}
 }
 
-struct OffsettableScrollView<T: View>: View {
+struct OffsettableScrollView<Content: View>: View {
 	let axes: Axis.Set
 	let showsIndicator: Bool
-	let onOffsetChanged: (CGPoint) -> Void
-	let content: T
+	let onOffsetChange: (CGPoint) -> Void
+	let content: Content
 
 	init(
 		axes: Axis.Set = .vertical,
 		showsIndicator: Bool = true,
-		onOffsetChanged: @escaping (CGPoint) -> Void = { _ in },
-		@ViewBuilder content: () -> T
+		onOffsetChange: @escaping (CGPoint) -> Void = { _ in },
+		@ViewBuilder _ content: () -> Content
 	) {
 		self.axes = axes
 		self.showsIndicator = showsIndicator
-		self.onOffsetChanged = onOffsetChanged
+		self.onOffsetChange = onOffsetChange
 		self.content = content()
 	}
 
 	var body: some View {
 		ScrollView(axes, showsIndicators: showsIndicator) {
-			GeometryReader { proxy in
-				Color.clear.preference(
-					key: OffsetPreferenceKey.self,
-					value: proxy.frame(
-						in: .named("ScrollViewOrigin")
-					).origin
-				)
-			}
-			.frame(width: 0, height: 0)
+			offsetReader
 			content
 				.padding(.top, -8)
 		}
 		.coordinateSpace(name: "ScrollViewOrigin")
-		.onPreferenceChange(
-			OffsetPreferenceKey.self,
-			perform: onOffsetChanged
-		)
+		.onPreferenceChange(OffsetPreferenceKey.self, perform: onOffsetChange)
+	}
+	
+	var offsetReader: some View {
+		GeometryReader { proxy in
+			Color.clear.preference(
+				key: OffsetPreferenceKey.self,
+				value: proxy.frame(in: .named("ScrollViewOrigin")).origin
+			)
+		}
+		.frame(width: 0, height: 0)
 	}
 }
