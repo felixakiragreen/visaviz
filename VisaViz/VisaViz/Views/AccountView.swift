ce4f5b0a0dcf206c8b8d7e21b93a646243215398
Felix Green <work@felix.green>
2022-08-29 19:40:08 -0400

    create a simple account view

--- VisaViz/VisaViz/Views/AccountView.swift
+++ VisaViz/VisaViz/Views/AccountView.swift
@@ -5,16 +5,56 @@
 //  Created by Felix Akira Green on 8/29/22.
 //
 
+import Atoms
 import SwiftUI
 
 struct AccountView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	@WatchStateObject(TweetArchiveAtom())
+	var archive
+
+	var body: some View {
+		VStack(alignment: .trailing) {
+			if archive.isLoaded {
+				Text("👋 \(archive.account.accountDisplayName)")
+				
+				Text("username: ")
+					.foregroundColor(Color(.grey, 500))
+					.font(.system(.caption, design: .monospaced))
+				+ Text("@\(archive.account.username)")
+				
+				Text("twitter ID: ")
+					.foregroundColor(Color(.grey, 500))
+					.font(.system(.caption, design: .monospaced))
+				+ Text("\(archive.account.accountId)")
+				
+				Text("user since: ")
+					.foregroundColor(Color(.grey, 500))
+					.font(.system(.caption, design: .monospaced))
+				+ Text("\(DateFormatter.full.string(from: archive.account.createdAt))")
+			}
+			else {
+				if let error = archive.anyError {
+					Text("Error: \(error.localizedDescription)")
+				}
+				else {
+					Text("Archive Not Loaded")
+				}
+			}
+		}
+		.padding(16)
+		.background(
+			RoundedRectangle(cornerRadius: 8, style: .continuous)
+				.fill(
+					Color(.grey, archive.isLoaded ? 800 : 900)
+				)
+		)
+	}
 }
 
 struct AccountView_Previews: PreviewProvider {
-    static var previews: some View {
-        AccountView()
-    }
+	static var previews: some View {
+		AccountView()
+			.embedAtomRoot()
+			.preferredColorScheme(.dark)
+	}
 }
