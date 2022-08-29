e9e435d65b3c5d6d76b2e48c6be3f35fd1a8b1c7
Felix Green <work@felix.green>
2022-08-29 19:41:01 -0400

    use the new archiveloadedivew

--- VisaViz/VisaViz/ContentView.swift
+++ VisaViz/VisaViz/ContentView.swift
@@ -12,46 +12,24 @@ struct ContentView: View {
 	@WatchStateObject(TweetArchiveAtom())
 	var archive
 
-	@WatchState(ScrollAtom())
-	var scroll
-
-	@State var hover: CGPoint?
-
 	var body: some View {
 		VStack {
 			ControlsView()
 				.padding()
 
-			ZStack {
-				GeometryReader { geo in
-					OffsettableScrollView(onOffsetChange: { point in
-						scroll = point.y
-					}) {
-						TweetCanvasView(
-							size: geo.size
-						)
-						.frame(maxWidth: .infinity, minHeight: geo.size.height)
-					}
-				} // GeometryReader
-				.frame(maxWidth: .infinity, maxHeight: .infinity)
-				
-				.mouseMove(isInside: { inside in
-					// print("is inside: \(inside)")
-					if inside == false {
-						hover = nil
-					}
-				}, onMove: { location in
-					// print("location: \(location)")
-					hover = location
-				})
-
-				if hover != nil {
-					TweetHoverView(hover: hover!)
+			VStack {
+				if archive.isLoaded {
+					ArchiveLoadedView()
+				} else {
+					EmptyView()
 				}
-			} // ZStack
+			}
+			.frame(maxWidth: .infinity, maxHeight: .infinity)
+
 		} // VStack
-		.background(Color(.grey, 900))
 		.frame(maxWidth: .infinity, maxHeight: .infinity)
+		.background(Color(.grey, 900))
+		.environment(\.colorScheme, .dark)
 	}
 }
 
